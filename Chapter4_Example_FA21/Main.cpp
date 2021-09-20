b3cbf7d5599ffe08eb97dcc0c68741fe5d909348
BuschMath <busch@iowacentral.edu>
2021-09-20 10:37:19 -0500

    File IO Example

--- Chapter4_Example_FA21/Main.cpp
+++ Chapter4_Example_FA21/Main.cpp
@@ -1,19 +1,31 @@
-#include <iostream>
+#include <fstream>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-	int age;
-	string name;
-	cout << "\nwhat is your age: ";
-	cin >> age;
-	cout << endl << "The age you entered is: " << age;
-	cin.ignore(128,'\n');
-	cout << "\nenter your name: ";
-	getline(cin, name);
-	cout << "\nThe name you entered is: " << name;
+	string data;
+	string data2;
+	string idata3;
+	string data4;
+	string inputFileName = "C:/Users/busch/OneDrive - Iowa Central Community College/Classes/C++/Presentations/Ch4Examples/ExampleInputFile.dat";
+	string outputFileName = "ExampleOutputFile.dat";
+	ifstream inFile;
+	ofstream outFile;
+
+	inFile.open(inputFileName);		// Assumes input file is in the project folder
+	outFile.open(outputFileName, ios::app);		// Places output file in the project folder
+
+	//getline(inFile, data);
+	inFile >> data >> idata3 >> data2 >> data4;
+	outFile << data << data2 << idata3 << data4;
+
+	cout << data << " " << data2 << " " << idata3 << " " << data4 << endl;
+
+	inFile.close();
+	outFile.close();
 
 	return 0;
 }
